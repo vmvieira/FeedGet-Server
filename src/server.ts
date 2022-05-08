ef6588bc070db993b771c618ef39a97ee3318f31
Vitor Machado Vieira <vitor.machado@provi.com.br>
2022-05-07 22:43:44 -0300

    preparing for deploy

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,11 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 200000 })
+);
+
 app.use(routes);
 
 app.listen(3001, () => console.log('Server running!'));
